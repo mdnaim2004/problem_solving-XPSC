56ae6ce5d21d1c91a6674e24f9558c1be88c0e6c
Md Naim <naim.cse2004@gmail.com>
2026-06-10 20:55:04 +0600

    add Seated Together solution and problem configuration

---
+++ codechef contest 6/Seated Together.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int x; cin >> x;
+    
+    if(x % 5 == 0)
+        cout << "No" <<"\n";
+    else
+        cout<< "Yes" <<"\n";
+    return 0;
+}
