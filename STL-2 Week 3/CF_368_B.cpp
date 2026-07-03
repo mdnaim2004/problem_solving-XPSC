a258c2167ec82e093978f9c6d1de4e66570d8986
mdnaim2004 <naim.cse2004@gmail.com>
2026-07-03 18:05:13 +0600

    refactor CF 368 B solution and add problem configuration

--- STL-2 Week 3/CF_368_B.cpp
+++ STL-2 Week 3/CF_368_B.cpp
@@ -1,22 +1,32 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main(){
+int main() {
     int n, m;
-    cin >> n >>m;
-    vector<int> arr(n+5);
-    for(int i=0; i<n; i++){
-        cin >> arr[i];
-    }
-    while(m--){
-        int l;
-        cin >>l;
-        set<int> s;
+    cin >> n >> m;
 
-        for(int i = l-1; i<n; i++){
-            s.insert(arr[i]);
-        }
-        cout << s.size() << "\n";
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    set<int> s;
+
+    for (int i = n - 1; i >= 0; i--) {
+        s.insert(arr[i]);
+        arr[i] = s.size();
+        cout << arr[i] << " ";
     }
-    return 0;
+
+    // while (m--) {
+    //     int p;
+    //     cin >> p;
+    //     cout << arr[p - 1] << '\n';
+    // }
 }
+// int main(){
+//     int n = 99999;
+//     set<int>s;
+//     s.insert(n);
+//     cout << s.size() <<"\n";
+// }
